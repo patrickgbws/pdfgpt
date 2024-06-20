d8e6cbcac39c86c6e0dfbac9d4e20ac52f2e38bc
OliverLy670 <oliverly670@gmail.com>
2024-06-20 09:35:52 +0800

    Modify code

--- types/siteConfig.ts
+++ types/siteConfig.ts
@@ -1,54 +1,49 @@
-import { IconType } from "react-icons"
+import {IconType} from "react-icons"
 
 export type AuthorsConfig = {
-  name: string
-  url: string
-  twitter?: string
-}
-export type ProductLink = {
-  url: string
-  name: string
+    name: string
+    url: string
+    twitter?: string
 }
 export type Link = {
-  name: string
-  href: string
-  icon: IconType
+    name: string
+    href: string
 }
 export type ThemeColor = {
-  media: string
-  color: string
+    media: string
+    color: string
 }
 export type SiteConfig = {
-  name: string
-  description: string
-  url: string
-  keywords: string[]
-  authors: AuthorsConfig[]
-  creator: string
-  ogImage: string
-  headerLinks: Link[]
-  metadataBase: URL | string
-  themeColors?: string | ThemeColor[]
-  defaultNextTheme?: string
-  icons: {
-    icon: string
-    shortcut?: string
-    apple?: string
-  }
-  openGraph: {
-    type: string
-    locale: string
+    name: string
+    description: string
     url: string
-    title: string
-    description: string
-    siteName: string
-    images?: string[]
-  },
-  twitter: {
-    card: string
-    title: string
-    description: string
-    images?: string[]
+    keywords: string[]
+    authors: AuthorsConfig[]
     creator: string
-  },
+    ogImage: string
+    headerLinks: Link[]
+    metadataBase: URL | string
+    themeColors?: string | ThemeColor[]
+    defaultNextTheme?: string
+    icons: {
+        icon: string
+        shortcut?: string
+        apple?: string
+    }
+    openGraph: {
+        type: string
+        locale: string
+        url: string
+        title: string
+        description: string
+        siteName: string
+        images?: string[]
+    },
+    twitter: {
+        card: string
+        title: string
+        description: string
+        images?: string[]
+        creator: string
+    },
 }
